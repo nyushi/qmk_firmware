1ffb9e00f15c976776a78947589fafa74f03bea1
Yushi Nakai <nyushi@users.noreply.github.com>
2018-11-04 19:52:40 +0900

    Update

--- keyboards/fortitude60_nyushi/keymaps/default/keymap.c
+++ keyboards/fortitude60_nyushi/keymaps/default/keymap.c
@@ -159,9 +159,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] =  LAYOUT( \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-  _______, RESET  , RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI,                   RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, _______, KC_BSPC, \
-  _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM,                   AG_SWAP, _______, BL_TOGG, BL_STEP, _______, _______, \
-  QWERTY,  COLEMAK, DVORAK,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, RESET  , RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI,                   RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, RGB_MOD, KC_BSPC, \
+  RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW,RGB_M_SN,RGB_M_K,                   RGB_M_X, RGB_M_G, RGB_M_T, _______, _______, _______, \
+  KC_LSFT, QWERTY,  COLEMAK, DVORAK,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
                     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______\
 )
 
@@ -172,7 +172,16 @@ uint32_t layer_state_set_user(uint32_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
 
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+/*
+  if(record->event.pressed){
+    rgblight_enable();
+  }else{
+    rgblight_disable();
+  }
+*/
+
   switch (keycode) {
     case QWERTY:
       if (record->event.pressed) {
