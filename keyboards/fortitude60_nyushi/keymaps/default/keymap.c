2b5ccf96220ebe2758554d3db569bc75c427944c
Yushi Nakai <nyushi@users.noreply.github.com>
2018-11-25 18:10:40 +0900

    Update shift with cursor

--- keyboards/fortitude60_nyushi/keymaps/default/keymap.c
+++ keyboards/fortitude60_nyushi/keymaps/default/keymap.c
@@ -141,7 +141,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,  \
   KC_GRV,  KC_1, KC_2,   KC_3,    KC_4,    KC_5,                       KC_6,    KC_7, KC_8,  KC_9,    KC_0,    KC_PAUSE, \
   KC_DEL,  KC_MS_L, KC_MS_D, KC_MS_R, KC_F4,   KC_F5,             KC_LEFT,KC_DOWN,  KC_UP, KC_RGHT, KC_UP,    KC_BSLS, \
-  _______, KC_ACL0, KC_ACL1, KC_ACL2,   KC_F10,  KC_F11,  _______, _______,  KC_PGDOWN,  KC_PGUP, KC_LEFT, KC_DOWN, KC_RGHT, _______, \
+  KC_LSFT, KC_ACL0, KC_ACL1, KC_ACL2,   KC_F10,  KC_F11,  _______, _______,  KC_PGDOWN,  KC_PGUP, KC_LEFT, KC_DOWN, KC_RGHT, _______, \
               _______, _______, KC_SPC, KC_BTN1, KC_LSFT, _______,  _______, _______, _______, _______\
 ),
 
