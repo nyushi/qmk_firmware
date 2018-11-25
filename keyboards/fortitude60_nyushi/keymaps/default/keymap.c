3ce2009e6b9f53b5111b9ad520d80a0c62f3d962
Yushi Nakai <nyushi@users.noreply.github.com>
2018-11-25 18:00:18 +0900

    Update

--- keyboards/fortitude60_nyushi/keymaps/default/keymap.c
+++ keyboards/fortitude60_nyushi/keymaps/default/keymap.c
@@ -139,10 +139,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,  \
-  KC_GRV,  KC_1, KC_MS_U,   KC_3,    KC_4,    KC_5,                       KC_6,    KC_SLCK, KC_PAUSE,    KC_9,    KC_SLCK,    KC_PAUSE, \
-  KC_DEL,  KC_MS_L, KC_MS_D, KC_MS_R, KC_F4,   KC_F5,             KC_F6,KC_PGUP,  KC_EQL, KC_UP, KC_UP,    KC_BSLS, \
-  _______, KC_ACL0, KC_ACL1, KC_ACL2,   KC_F10,  KC_F11,  _______, _______,  KC_F12,  KC_PGDOWN, KC_LEFT, KC_DOWN, KC_RGHT, _______, \
-              _______, _______, KC_SPC, KC_BTN1, KC_BTN2, _______,  _______, _______, _______, _______\
+  KC_GRV,  KC_1, KC_2,   KC_3,    KC_4,    KC_5,                       KC_6,    KC_7, KC_8,  KC_9,    KC_0,    KC_PAUSE, \
+  KC_DEL,  KC_MS_L, KC_MS_D, KC_MS_R, KC_F4,   KC_F5,             KC_LEFT,KC_DOWN,  KC_UP, KC_RGHT, KC_UP,    KC_BSLS, \
+  _______, KC_ACL0, KC_ACL1, KC_ACL2,   KC_F10,  KC_F11,  _______, _______,  KC_PGDOWN,  KC_PGUP, KC_LEFT, KC_DOWN, KC_RGHT, _______, \
+              _______, _______, KC_SPC, KC_BTN1, KC_LSFT, _______,  _______, _______, _______, _______\
 ),
 
 /* Adjust (Lower + Raise)
@@ -159,11 +159,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               `---------------------------------------------------------------------'
  */
 [_ADJUST] =  LAYOUT( \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-  _______, RESET  , RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI,                   RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, RGB_MOD, KC_BSPC, \
-  RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW,RGB_M_SN,RGB_M_K,                   RGB_M_X, RGB_M_G, RGB_M_T, _______, _______, _______, \
-  KC_LSFT, QWERTY,  COLEMAK, DVORAK,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-                    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______\
+  RESET,   RGB_TOG, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI,           RGB_VAD,   RGB_VAI,   _______,   _______,  KC_SLCK,  KC_PAUSE, \
+  _______, _______  , KC_MS_U, _______, _______, _______,                   _______, _______, _______, _______, KC_PGDOWN, KC_PGUP, \
+  _______, KC_MS_L, KC_MS_D, KC_MS_R,_______,_______,                   KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______, \
+  RGB_M_P, RGB_M_B,  RGB_M_R, RGB_M_SW,  RGB_M_SN, RGB_M_K, _______, _______, RGB_M_X, RGB_M_G, RGB_M_T, _______, _______, _______, \
+                    _______, _______, _______, _______, _______, _______, _______, _______, KC_BTN1, KC_BTN2\
 )
 
 
