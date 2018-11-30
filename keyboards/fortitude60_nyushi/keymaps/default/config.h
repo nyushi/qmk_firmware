82543bf57b825e587989665234dbfd795bb7280d
Yushi Nakai <nyushi@users.noreply.github.com>
2018-11-30 18:59:35 +0900

    Adjust delay and LED deco

--- keyboards/fortitude60_nyushi/keymaps/default/config.h
+++ keyboards/fortitude60_nyushi/keymaps/default/config.h
@@ -36,6 +36,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* #define RGBLIGHT_VAL_STEP 8 */
 
 #define TAPPING_FORCE_HOLD
+#define TAPPING_TERM 150
+
 #define RGBLIGHT_EFFECT_BREATHING
 #define RGBLIGHT_EFFECT_RAINBOW_MOOD
 #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
