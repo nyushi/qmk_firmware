40c164286ab68ae6162cee60e952368631ba3a90
Yushi Nakai <nyushi@users.noreply.github.com>
2019-01-11 17:05:16 +0900

    Updare

--- keyboards/fortitude60_nyushi/keymaps/default/keymap.c
+++ keyboards/fortitude60_nyushi/keymaps/default/keymap.c
@@ -61,9 +61,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL, \
- KC_LCANDE,KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  _______,KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_ESC, KC_BSPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLASH , \
-               KC_LALTE,KC_LGUI,  KC_LSANDS,LOWERE,KC_LCTRL, KC_ENT ,  RAISEK,KC_RSANDS, KC_RGUI, KC_RALTK\
+               KC_LALT ,KC_LGUIE,  KC_LSANDS,KC_LCTRL,LOWER, RAISE, KC_ENT,KC_RSANDS, KC_RGUIK, KC_RALT\
 ),
 
 /* Colemak
@@ -123,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT( \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_EQL, \
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PLUS,  \
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,  \
   _______,  _______, _______, _______, _______, KC_LBRC,                   KC_RBRC, _______, _______, _______, KC_LBRC, KC_RBRC, \
   _______, _______, _______, _______, _______, KC_LCBR,  _______, _______,KC_RCBR,_______,_______,_______, _______, _______,\
                     _______, _______, KC_LSANDS, _______, _______, _______,  _______, KC_RSANDS, _______, _______\
