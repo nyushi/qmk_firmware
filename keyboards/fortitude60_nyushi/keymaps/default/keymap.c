0b42116d2150fe2811d92e31e40dc247d20d226c
Yushi Nakai <nyushi@users.noreply.github.com>
2019-01-21 17:27:10 +0900

    Update

--- keyboards/fortitude60_nyushi/keymaps/default/keymap.c
+++ keyboards/fortitude60_nyushi/keymaps/default/keymap.c
@@ -61,9 +61,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL, \
-  _______,KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LCANDE,KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_ESC, KC_BSPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLASH , \
-               KC_LALT ,KC_LGUIE,  KC_LSANDS,KC_LCTRL,LOWER, RAISE, KC_ENT,KC_RSANDS, KC_RGUIK, KC_RALT\
+               KC_LALTE ,KC_LGUI,  KC_LSANDS,LOWER,KC_LCTRL, KC_ENT, RAISE,KC_RSANDS, KC_RGUI, KC_RALTK\
 ),
 
 /* Colemak
@@ -124,7 +124,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT( \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_EQL, \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,  \
-  _______,  _______, _______, _______, _______, KC_LBRC,                   KC_RBRC, _______, _______, _______, KC_LBRC, KC_RBRC, \
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, \
   _______, _______, _______, _______, _______, KC_LCBR,  _______, _______,KC_RCBR,_______,_______,_______, _______, _______,\
                     _______, _______, KC_LSANDS, _______, _______, _______,  _______, KC_RSANDS, _______, _______\
 ),
@@ -145,7 +145,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC, \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS, \
-  KC_DEL,  _______, _______, _______, _______,   _______,         KC_LEFT,KC_DOWN,  KC_UP, KC_RGHT, KC_UP,    KC_BSLS, \
+  KC_DEL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSLS, \
   KC_LSFT, KC_ACL0, KC_ACL1, KC_ACL2,   KC_F10,  KC_F11,  _______, _______,  _______,  KC_PGDOWN, KC_PGUP, _______, _______, _______, \
               _______, _______, KC_LSANDS, KC_BTN1, KC_LSFT, _______,  _______, KC_RSANDS, _______, _______\
 ),
@@ -165,8 +165,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] =  LAYOUT( \
   RESET,   RGB_TOG, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI,           RGB_VAD,   RGB_VAI,   RGB_M_P,   _______,  KC_SLCK,  KC_PAUSE, \
-  KC_F1, KC_F2 , KC_F3, KC_F4, KC_F5, KC_F6,                   KC_F7, KC_F8, KC_F9, KC_F10, KC_PGDOWN, KC_PGUP, \
-  KC_F11, KC_F12, _______,_______,_______, _______,                 KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______, \
+  KC_F1, KC_F2 , KC_F3, KC_F4, KC_F5, KC_F6,                   _______, _______, _______, KC_LBRC, KC_RBRC, _______, \
+  KC_F7, KC_F8, KC_F9,KC_F10,KC_F11, KC_F12,                 KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______, \
   KC_LSFT, RGB_M_P,  RGB_M_B, RGB_M_R,  RGB_M_K, RGB_MOD, _______, _______, RGB_RMOD, KC_BTN1,KC_BTN2, _______, _______, _______, \
                     _______, _______, KC_SPC, _______, _______, _______, _______, KC_SPC, KC_BTN1, KC_BTN2\
 ),
