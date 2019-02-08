ff6ad34250f522eee752c14e8e036ff24f51e4e1
Yushi Nakai <nyushi@users.noreply.github.com>
2019-02-08 10:39:05 +0900

    Update

--- keyboards/fortitude60_nyushi/keymaps/default/keymap.c
+++ keyboards/fortitude60_nyushi/keymaps/default/keymap.c
@@ -147,7 +147,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS, \
   KC_DEL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS, \
   KC_LSFT, KC_ACL0, KC_ACL1, KC_ACL2,   KC_F10,  KC_F11,  _______, _______,  _______,  KC_PGDOWN, KC_PGUP, _______, _______, _______, \
-              _______, _______, KC_LSANDS, LOWER, KC_LSFT, _______,  RAISE, KC_RSANDS, _______, _______\
+              _______, _______, KC_LSANDS, LOWER, MOVE, _______,  RAISE, KC_RSANDS, _______, _______\
 ),
 
 /* Adjust (Lower + Raise)
@@ -176,7 +176,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______  , _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
   _______,  _______, _______,_______,_______, _______,              KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______, \
   KC_LSFT, _______,  _______, _______,  _______, _______, _______, _______, _______, KC_PGDOWN, KC_PGUP, _______, _______, _______, \
-                    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______\
+                    _______, _______, KC_SPC, LOWER, MOVE, _______, RAISE, KC_SPC, _______, _______\
 )
 
 
