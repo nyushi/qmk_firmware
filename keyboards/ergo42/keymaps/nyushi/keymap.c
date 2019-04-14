c5f98bcb668bb0207e9bd24444df7ec6d7c9fd15
Yushi Nakai <nyushi@users.noreply.github.com>
2019-04-14 23:01:26 +0900

    Update

--- keyboards/ergo42/keymaps/nyushi/keymap.c
+++ keyboards/ergo42/keymaps/nyushi/keymap.c
@@ -38,10 +38,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT( \
-  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,  _______,  KC_Y,    KC_U,       KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,\
+  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,  ADJUST,  KC_Y,    KC_U,       KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,\
   KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,   KC_G,  _______,  KC_H,    KC_J,       KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLASH,\
-  KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,  KC_BSPC,  KC_N,       KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______, _______, \
-  KC_ESC, _______, KC_LALTE,KC_LGUI,KC_LSANDS, LOWERD, _______,   RAISEE,  KC_RSANDS  , KC_RGUI, KC_RALTK, _______, _______, ADJUST
+  KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,  KC_BSPC,  KC_N,       KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______, KC_BSPC, \
+  KC_ESC, KC_LALTE, KC_LALTE,KC_LGUI,KC_LSANDS, LOWERD, KC_ESC,   RAISEE,  KC_RSANDS  , KC_RGUI, KC_RALTK, KC_RALTK, _______, KC_ENT
 ),
 
 /* RAISE
