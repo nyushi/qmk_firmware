6e07b75c2603eea02b9423283dda9f39a8fbf300
Yushi Nakai <nyushi@users.noreply.github.com>
2019-04-23 23:31:26 +0900

    Update Functions

--- keyboards/ergo42/keymaps/nyushi/keymap.c
+++ keyboards/ergo42/keymaps/nyushi/keymap.c
@@ -38,10 +38,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT( \
-  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,  ADJUST,  KC_Y,    KC_U,       KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,\
+  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,  _______,  KC_Y,    KC_U,       KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,\
   KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,   KC_G,  _______,  KC_H,    KC_J,       KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLASH,\
-  KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,  KC_BSPC,  KC_N,       KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______, KC_BSPC, \
-  KC_ESC, KC_LALTE, KC_LALTE,KC_LGUI,KC_LSANDS, LOWERD, KC_ESC,   RAISEE,  KC_RSANDS  , KC_RGUI, KC_RALTK, KC_RALTK, _______, KC_ENT
+  KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,  KC_BSPC,  KC_N,       KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______, _______, \
+  KC_ESC, KC_LALTE, KC_LALTE,KC_LGUI,KC_LSANDS, LOWERD, KC_ESC,   RAISEE,  KC_RSANDS  , KC_RGUI, KC_RALTK, KC_RALTK, _______, ADJUST
 ),
 
 /* RAISE
@@ -121,8 +121,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_ADJUST] = LAYOUT( \
-  RESET,   KC_A, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_SLCK, KC_PAUSE, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  RESET,   KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_SLCK, KC_PAUSE, _______, _______, _______, _______, KC_BSPC, \
+  _______, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, _______, _______, _______, _______, _______, KC_ENT, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
 ),
