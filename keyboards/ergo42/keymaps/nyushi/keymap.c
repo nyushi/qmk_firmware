7276b9b50d206dc4af7b8973bdc1f4b1451df9bb
Yushi Nakai <nyushi@users.noreply.github.com>
2019-06-09 07:21:13 +0900

    Update

--- keyboards/ergo42/keymaps/nyushi/keymap.c
+++ keyboards/ergo42/keymaps/nyushi/keymap.c
@@ -2,6 +2,10 @@
 
 extern keymap_config_t keymap_config;
 
+enum custom_keycodes {
+  MY_BAKUHA = SAFE_RANGE,
+};
+
 #define _QWERTY 0
 #define _LOWER 1
 #define _RAISE 2
@@ -17,6 +21,8 @@ extern keymap_config_t keymap_config;
 #define KC_RSANDS MT(MOD_RSFT, KC_SPC)
 #define KC_LALTE MT(MOD_LALT, KC_LANG2)
 #define KC_RALTK MT(MOD_RALT, KC_LANG1)
+#define KC_LGUIE MT(MOD_LGUI, KC_LANG2)
+#define KC_RGUIK MT(MOD_RGUI, KC_LANG1)
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 #define ADJUST MO(_ADJUST)
@@ -38,10 +44,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT( \
-  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,  _______,  KC_Y,    KC_U,       KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,\
-  KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,   KC_G,  _______,  KC_H,    KC_J,       KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLASH,\
-  KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,  KC_BSPC,  KC_N,       KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______, _______, \
-  KC_ESC, KC_LALTE, KC_LALTE,KC_LGUI,KC_LSANDS, LOWERD, KC_ESC,   RAISEE,  KC_RSANDS  , KC_RGUI, KC_RALTK, KC_RALTK, _______, ADJUST
+  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,  _______/*MY_BAKUHA*/,  KC_Y,    KC_U,       KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,\
+  KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,   KC_G,  KC_ENT,               KC_H,    KC_J,       KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLASH,\
+  KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,  KC_BSPC,              KC_N,       KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_HOME, _______, \
+  ADJUST, _______, KC_LALTE,KC_LGUIE,KC_LSANDS, LOWERD, KC_ESC,             RAISEE,  KC_RSANDS  , KC_RGUIK, KC_RALTK, _______, KC_END, ADJUST
 ),
 
 /* RAISE
@@ -58,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL, \
   _______, _______, _______, _______, _______, _______,  _______, KC_LEFT, KC_DOWN,KC_UP,KC_RIGHT,  _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______,  _______, KC_ENT, KC_SPC,  _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______,  _______, _______, KC_PGDOWN,  KC_PGUP, _______, _______, _______, _______, \
   _______, _______, _______, _______, KC_SPC, _______, _______, _______, KC_SPC, _______, _______, _______, _______, _______
 ),
 
@@ -77,8 +83,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_LOWER] = LAYOUT( \
   KC_TILD, KC_EXLM, KC_AT,    KC_HASH, KC_DLR,  KC_PERC, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS,\
-  _______, KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,  KC_MS_LEFT,KC_MS_DOWN,KC_MS_UP,KC_MS_RIGHT, KC_LCBR, KC_RCBR, KC_PIPE, \
-  _______,  KC_F7,  KC_F8,    KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_MS_BTN2, KC_MS_WH_UP, KC_MS_WH_DOWN, KC_PGUP, KC_HOME, KC_UP,   KC_END, \
+  _______, _______, _______,  _______, _______, _______, _______,  KC_MS_LEFT,KC_MS_DOWN,KC_MS_UP,KC_MS_RIGHT, KC_LCBR, KC_RCBR, KC_PIPE, \
+  _______, _______, _______,  _______, _______, _______, _______, KC_MS_BTN2, KC_MS_WH_UP, KC_MS_WH_DOWN, KC_PGUP, KC_HOME, KC_UP,   KC_END, \
   _______, _______, _______, _______,  KC_SPC, _______, _______, KC_MS_BTN1, KC_SPC, _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT \
 ),
 
@@ -121,8 +127,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_ADJUST] = LAYOUT( \
-  RESET,   KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_SLCK, KC_PAUSE, _______, _______, _______, _______, KC_BSPC, \
-  _______, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, _______, _______, _______, _______, _______, KC_ENT, \
+  RESET,   KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_PAUSE, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, \
+  _______, _______, _______, _______, _______, _______, KC_SLCK, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
 ),
@@ -130,3 +136,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case MY_BAKUHA:
+      if (record->event.pressed) {
+        // when pressed
+        SEND_STRING("doka-nn\n");
+      } else {
+        // when released
+      }
+      break;
+
+  }
+  return true;
+};
